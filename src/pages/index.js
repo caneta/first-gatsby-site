d81026be740cdbb419fba5e4b5da48212ae52ba2
caneta <canduc17@gmail.com>
2021-08-18 16:22:25 +0200

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 // styles
 // const pageStyles = {
@@ -132,6 +133,11 @@ const IndexPage = () => {
     <main>
       <Layout pageTitle="Home Page">
         <p>I&apos;m making this by following the Gatsby Tutorial.</p>
+
+        <StaticImage
+          alt="Spaghetti with clams, parmigiana and white wine"
+          src="../images/italian-food.jpg"
+        />
       </Layout>
     </main>
   );
